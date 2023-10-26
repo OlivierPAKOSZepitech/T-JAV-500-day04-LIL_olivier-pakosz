5667581f0f2537318494def789c96cba914ce25e
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-26 12:36:09 +0200

    ajout task 3

--- Animal.java
+++ Animal.java
@@ -83,6 +83,30 @@ public class Animal {
         }
     }
 
+   
+public class Cat extends Animal {
+    private String color;
+
+    public Cat(String name, String color) {
+        super(name, 4, Type.MAMMAL);
+        this.color = color != null ? color : "grey";
+        System.out.println(getName() + ": MEEEOOWWWW");
+    }
+
+    public Cat(String name) {
+        this(name, null);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void meow() {
+        System.out.println(getName() + " the " + getColor() + " kitty is meowing.");
+    }
+}
+
+
     // Example main method
     public static void main(String[] args) {
         Animal isidore = new Animal("Isidore", 4, Type.MAMMAL);
